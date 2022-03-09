0e2970e30d98d925fdf0260898a6f1351992f45d
du99cy <du99cy@gmail.com>
2022-03-09 14:32:49 +0700

    'bind_login_to_btn'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,25 +4,30 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren:()=> import('@modules/home/home.module').then(m=>m.HomeModule)
+    loadChildren: () =>
+      import('@modules/home/home.module').then((m) => m.HomeModule),
   },
   {
-
     path: 'course-detail',
-    loadChildren:()=> import('../../src/app/modules/course-detail/course-detail.module').then(m=>m.CourseDetailModule)
+    loadChildren: () =>
+      import('../../src/app/modules/course-detail/course-detail.module').then(
+        (m) => m.CourseDetailModule
+      ),
   },
-
+  {
     path: 'login',
-    loadChildren:()=> import('@modules/login/login.module').then(m=>m.loginModule)
+    loadChildren: () =>
+      import('@modules/login/login.module').then((m) => m.loginModule),
   },
   {
     path: 'sign-up',
-    loadChildren:()=> import('@modules/sign-up/sign-up.module').then(m=>m.signUpModule)
-
+    loadChildren: () =>
+      import('@modules/sign-up/sign-up.module').then((m) => m.signUpModule),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { FooterComponent } from '@core/components/footer/footer.component';
 import { HeaderComponent } from '@core/components/header/header.component';
 import { CoreModule } from '@core/core.module';
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule} from './app-routing.module'
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
