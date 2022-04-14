323d28fae5a3eef8623f3c6f0503b3df10c5b0f2
Hà Quang Huy <huyhq@aiacademy.edu.vn>
2022-04-14 17:06:23 +0700

    huy-update

--- src/app/modules/course-manage/course-manage-routing.module.ts
+++ src/app/modules/course-manage/course-manage-routing.module.ts
@@ -4,6 +4,11 @@ import { CurriculumComponent } from './modules/curriculum/components/curriculum/
 import { IntendedLearnersComponent } from './modules/intended-learners/intended-learners.component';
 import { LayoutManageComponent } from './core/components/layout-manage/layout-manage.component'
 import { CourseLandingPageComponent } from './modules/course-landing-page/course-landing-page.component';
+import { CourseMessagesComponent } from './modules/course-messages/course-messages.component';
+import { CourseStructureComponent } from './modules/course-structure/course-structure.component';
+import { FilmEditComponent } from './modules/film-edit/film-edit.component';
+import { PricingComponent } from './modules/pricing/pricing.component';
+import { SetupTestComponent } from './modules/setup-test/setup-test.component';
 
 
 const routes: Routes = [
@@ -22,6 +27,26 @@ const routes: Routes = [
       {
         path: 'course-landing-page', // child route path
         loadChildren: () =>import('./modules/course-landing-page/course-landing-page.module').then(m=>m.CourseLandingPageModule)
+      },
+      {
+        path: 'course-messages', // child route path
+        component: CourseMessagesComponent
+      },
+      {
+        path: 'course-structure', // child route path
+        component: CourseStructureComponent
+      },
+      {
+        path: 'film-edit', // child route path
+        component: FilmEditComponent
+      },
+      {
+        path: 'pricing', // child route path
+        component: PricingComponent
+      },
+      {
+        path: 'setup-test', // child route path
+        component: SetupTestComponent
       },
       
   ]
---
+++ src/app/modules/course-manage/modules/setup-test/setup-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-setup-test',
+  templateUrl: './setup-test.component.html',
+  styleUrls: ['./setup-test.component.scss']
+})
+export class SetupTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
