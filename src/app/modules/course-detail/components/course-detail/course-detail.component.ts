451ea5698e6670663a2818c282b45a14a869ab03
Nguyễn Quang Dự <dunq@aiacademy.edu.vn>
2022-05-14 13:47:36 +0700

    update

--- src/app/modules/course-detail/components/course-detail/course-detail.component.ts
+++ src/app/modules/course-detail/components/course-detail/course-detail.component.ts
@@ -1,10 +1,13 @@
 import { query } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { User } from '@core/authentication/user';
 import { scrollToTopPage } from '@core/utilities/helpers';
 import { CourseDetailService } from '@modules/course-detail/services/course-detail.service';
 import { RegisterCourseService } from '@modules/course-detail/services/register-course.service';
+import { Content } from '@modules/course-manage/modules/curriculum/models/content';
 import { CourseService } from '@modules/home/services/course.service';
+import { Course } from '@modules/new-course-creation/models/course';
 import { api_urls } from '@shared/configs/api_url';
 import Swal from 'sweetalert2';
 @Component({
@@ -13,9 +16,14 @@ import Swal from 'sweetalert2';
   styleUrls: ['./course-detail.component.scss'],
 })
 export class CourseDetailComponent implements OnInit {
+
+
   router: any;
+
   courseId:string
-  courses: any;
+  course: Course;
+  instructor:User
+  contents:Array<Content>
   panelOpenState = false;
   listTopic: any;
   listTopicUser: any;
@@ -24,24 +32,24 @@ export class CourseDetailComponent implements OnInit {
 
   button_dis: any;
 
-  buttons_data:any = {
-    no_registration: {
-      name: 'Đăng kí',
-      event: () => {
-        this.register();
-      },
-    },
-    registrated: {
-      name: 'Tham gia',
-      event: () => {
-        alert("chức năng hoàn thiện sau !!!")
-      },
-    },
-    not_accept: {
-      name: 'Chờ phê duyệt',
-      event: () => {},
-    },
-  };
+  // buttons_data:any = {
+  //   no_registration: {
+  //     name: 'Đăng kí',
+  //     event: () => {
+  //       this.register();
+  //     },
+  //   },
+  //   registrated: {
+  //     name: 'Tham gia',
+  //     event: () => {
+  //       alert("chức năng hoàn thiện sau !!!")
+  //     },
+  //   },
+  //   not_accept: {
+  //     name: 'Chờ phê duyệt',
+  //     event: () => {},
+  //   },
+  // };
   constructor(
     private activateRoute: ActivatedRoute,
     private courseService: CourseService,
@@ -80,35 +88,44 @@ export class CourseDetailComponent implements OnInit {
       'https://course.aiacademy.edu.vn/images'
     );
   }
-  register() {
-    this.registerCourse
-      .registerCourse(this.courses[0]._id)
-      .subscribe((course) => {
-        this.button_dis = this.buttons_data.not_accept
-        Swal.fire({
-          position: 'center',
-          icon: 'success',
-          title: 'Bạn đã đăng kí thành công khóa học',
-          showConfirmButton: false,
-          timer: 1500,
-        });
-        this.openRegister = false;
-      });
-  }
+  // register() {
+  //   this.registerCourse
+  //     .registerCourse(this.courses[0]._id)
+  //     .subscribe((course) => {
+  //       this.button_dis = this.buttons_data.not_accept
+  //       Swal.fire({
+  //         position: 'center',
+  //         icon: 'success',
+  //         title: 'Bạn đã đăng kí thành công khóa học',
+  //         showConfirmButton: false,
+  //         timer: 1500,
+  //       });
+  //       this.openRegister = false;
+  //     });
+  // }
 
-  assignBtnByStatus(status_name: string) {
-    if (status_name == null) return this.buttons_data.no_registration;
-    else if (status_name == 'DONGY') return this.buttons_data.registrated;
-    else return this.buttons_data.not_accept;
-  }
+  // assignBtnByStatus(status_name: string) {
+  //   if (status_name == null) return this.buttons_data.no_registration;
+  //   else if (status_name == 'DONGY') return this.buttons_data.registrated;
+  //   else return this.buttons_data.not_accept;
+  // }
 
   getCourseDetailsSSE(courseId:string){
     let source = new EventSource(`${api_urls.LOCAL_API_URL}/course/${this.courseId}/data-stream`);
     source.addEventListener('message', (message) => {
 
-      let myData = JSON.parse(message.data);
-      console.log(myData)
-      // source.close()
+      let res = JSON.parse(message.data);
+      let data = res?.data
+      if(res?.data_name == "course")
+        this.course = data
+      else if(res?.data_name == "user")
+        this.instructor = data
+      else{
+        this.contents = data
+        source.close()
+      }
+
+
     });
     source.addEventListener('end', function (event) {
       console.log('Handling end....');
@@ -116,4 +133,6 @@ export class CourseDetailComponent implements OnInit {
     });
 
   }
+
+
 }
