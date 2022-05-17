16c577a26cdb94ca72612c6622ec039815e81888
Hà Quang Huy <huyhq@aiacademy.edu.vn>
2022-05-18 05:44:54 +0700

    huy

--- src/app/modules/coding-exam/components/coding-exam/coding-exam.component.ts
+++ src/app/modules/coding-exam/components/coding-exam/coding-exam.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { TestcaseCheck } from '@modules/coding-exam/models/testcase-check';
 import { CodingExamService } from '@modules/coding-exam/services/coding-exam.service';
 import { BehaviorSubject, map, Observable } from 'rxjs';
+import { OptionCodingEditorComponent } from '../option-coding-editor/option-coding-editor.component';
 
 @Component({
   selector: 'my-app',
@@ -14,34 +16,37 @@ import { BehaviorSubject, map, Observable } from 'rxjs';
 export class CodingExamComponent implements OnInit {
   dataObservable: Observable<any>;
   contentId: string;
-  // fg: any;
-  // list_language = ['python', 'cpp', 'java'];
-  // list_theme = ['vs-light', 'vs-dark'];
-  // format = '.py';
-  // listfileType = {
-  //   python: '.py',
-  //   cpp: '.cpp',
-  //   java: '.java',
-  //   javascript: '.js',
-  // };
-  // background_color = '';
-  // text_color = '';
+  is_get_testcase_result = false;
+  contentLoaded_scripts = false;
+  fg: any;
+  list_language = ['python', 'java'];
+  list_theme = ['vs-light', 'vs-dark'];
+  format = '.java';
+  listfileType = {
+    python: '.py',
+    java: '.java',
+    javascript: '.js',
+  };
+  background_color = '';
+  text_color = '';
   editorOptions = {
     theme: 'vs-dark',
     language: 'java',
     automaticLayout: true,
   };
-  //LanguageSubject$ = new BehaviorSubject<string>(this.editorOptions.language);
-  // back() {
-  //   window.history.back();
-  // }
+  LanguageSubject$ = new BehaviorSubject<string>(this.editorOptions.language);
+  back() {
+    window.history.back();
+  }
   onInit(editor: any) {
     let line = editor.getPosition();
   }
 
   constructor(
     private codingExamService: CodingExamService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    public dialog: MatDialog,
+    private elRef: ElementRef,
   ) {}
 
   ngOnInit(): void {
@@ -65,5 +70,56 @@ export class CodingExamComponent implements OnInit {
     })
 
   }
+  openDialog(): void {
+    const dialogRef = this.dialog.open(OptionCodingEditorComponent, {
+      data: this.editorOptions,
+      autoFocus: false,
+      restoreFocus: false,
+    });
 
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+
+        this.editorOptions = {
+          theme: result.theme,
+          language: result.language,
+          automaticLayout: true,
+        };
+        let lg = this.editorOptions.language;
+        this.format = (this.listfileType as any)[lg];
+        this.changeColor();
+        this.updateCustomProperty();
+        //load again template of code
+        //use for mat-spiner
+        this.contentLoaded_scripts = false;
+
+        this.LanguageSubject$.next(this.editorOptions.language);
+
+        //reload test case
+        this.is_get_testcase_result = false
+      }
+    });
+  }
+  updateCustomProperty() {
+    this.elRef.nativeElement.style.setProperty(
+      '--background_color',
+      `${this.background_color}`
+    );
+    this.elRef.nativeElement.style.setProperty(
+      '--text_color',
+      `${this.text_color}`
+    );
+  }
+  changeColor() {
+    if (this.editorOptions.theme === 'vs-dark') {
+      this.background_color = '#1e1e1e';
+      this.text_color = 'white';
+    }
+    if (this.editorOptions.theme === 'vs-light') {
+      this.background_color = 'white';
+      this.text_color = 'black';
+    }
+  }
 }
+
+
