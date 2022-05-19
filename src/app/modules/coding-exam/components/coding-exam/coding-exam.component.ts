84c5af91a569cbc44fd6ef1244b2de8686d89e28
Nguyễn Quang Dự <dunq@aiacademy.edu.vn>
2022-05-19 13:51:06 +0700

    'update'

--- src/app/modules/coding-exam/components/coding-exam/coding-exam.component.ts
+++ src/app/modules/coding-exam/components/coding-exam/coding-exam.component.ts
@@ -14,6 +14,8 @@ import { OptionCodingEditorComponent } from '../option-coding-editor/option-codi
   styleUrls: [`./coding-exam.component.scss`],
 })
 export class CodingExamComponent implements OnInit {
+  compile_error = false
+  compile_err_mes  =''
   dataObservable: Observable<any>;
   contentId: string;
   is_get_testcase_result = false;
@@ -21,7 +23,7 @@ export class CodingExamComponent implements OnInit {
   fg: any;
   list_language = ['python', 'java'];
   list_theme = ['vs-light', 'vs-dark'];
-  format = '.java';
+  format = '.py';
   listfileType = {
     python: '.py',
     java: '.java',
@@ -31,7 +33,7 @@ export class CodingExamComponent implements OnInit {
   text_color = '';
   editorOptions = {
     theme: 'vs-dark',
-    language: 'cpp',
+    language: 'python',
     automaticLayout: true,
   };
   LanguageSubject$ = new BehaviorSubject<string>(this.editorOptions.language);
@@ -40,6 +42,7 @@ export class CodingExamComponent implements OnInit {
   }
   onInit(editor: any) {
     let line = editor.getPosition();
+
   }
 
   constructor(
@@ -52,21 +55,38 @@ export class CodingExamComponent implements OnInit {
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       this.contentId = params['content_id'];
-      this.dataObservable = this.codingExamService
-        .getFunctionTemplate(this.contentId, this.editorOptions.language)
-        .pipe(
-          map((res) => {
-
-            return res.data;
-          })
-        );
+      this.dataObservable = this.getFunctionTemplate(this.contentId)
     });
+
+    this.LanguageSubject$.subscribe(language_name => {
+      this.dataObservable = this.getFunctionTemplate(this.contentId)
+    })
+
+
+  }
+
+  getFunctionTemplate(contentId: string){
+    return this.codingExamService
+    .getFunctionTemplate(contentId, this.editorOptions.language)
+    .pipe(
+      map((res) => {
+
+        return res.data;
+      })
+    );
   }
 
   runTest(codeEditor:any,data:any){
     let scripts = codeEditor._value
-    this.codingExamService.runTestcase(this.contentId,this.editorOptions.language,scripts).subscribe(res=>{
-      data.testcase_list = res.data
+    this.codingExamService.runTestcase(this.contentId,this.editorOptions.language,scripts).subscribe((res:any)=>{
+      console.log(res)
+      if (res.data.is_compile_error)
+      {
+        this.compile_error = true
+        this.compile_err_mes  = res.data.detail
+      }
+      else
+        data.testcase_list = res.data
     })
 
   }
